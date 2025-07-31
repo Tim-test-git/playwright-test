bd8d2b9a0b49cd9baedf010975e8bb1d096b81d8
Tim <tchisleacov@griddynamics.com>
2025-07-31 09:10:43 +0300

    new change

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -8,14 +8,6 @@ test('test', async ({ page }) => {
   await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'Community' })).toBeVisible();
 });
-test('test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'API' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Community' })).toBeVisible();
-});
 
 test('test2', async ({ page }) => {
   await page.goto('https://playwright.dev/');

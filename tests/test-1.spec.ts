60a3e90a7e081fa00fc4a5c044a7a9678f96a79b
Tim <tchisleacov@griddynamics.com>
2025-07-31 09:20:01 +0300

    add describe and beforEach

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,29 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'API' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Community' })).toBeVisible();
-});
+test.describe('Main Page Test', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+  });
+  test('test', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'API' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Community' })).toBeVisible();
+  });
 
-test('test2', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('test2', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toContainText(
+      'Playwright',
+    );
 
-  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toContainText(
-    'Playwright',
-  );
+    await expect(page.getByRole('link', { name: 'Docs' })).toContainText('Docs');
+  });
 
-  await expect(page.getByRole('link', { name: 'Docs' })).toContainText('Docs');
-});
-
-test('test3', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toHaveAttribute(
-    'href',
-    '/',
-  );
+  test('test3', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toHaveAttribute(
+      'href',
+      '/',
+    );
+  });
 });
